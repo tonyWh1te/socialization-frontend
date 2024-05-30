a7dd8d4c7119b97e9bc1d9793883c536ccb68f9c
tonyWh1te <zmyshenko.valery@bk.ru>
2024-05-31 02:05:11 +0300

    feat: added display of some test questions

--- src/modules/PassTest/components/QuestionItem/QuestionItem.jsx
+++ src/modules/PassTest/components/QuestionItem/QuestionItem.jsx
@@ -1,18 +1,45 @@
-import { TestCard, InputText } from '../../../../UI';
+import { TestCard, InputText, FormikCheckbox } from '../../../../UI';
 import styles from './QuestionItem.module.css';
 
-const QuestionItem = ({ question }) => {
+const QuestionItem = ({ question, qIndex }) => {
   const renderQuestionContent = (q) => {
     switch (q.type) {
       case 'text':
-        break;
+        return (
+          <InputText
+            type="text"
+            name={`answers[${qIndex}]`}
+            required={q.required}
+          />
+        );
+
+      case 'checkbox':
+        return (
+          <FormikCheckbox
+            label="Вариант ответа"
+            checkboxProps={{
+              value: '4',
+            }}
+            name={`answers[${qIndex}]`}
+            required={q.required}
+          />
+        );
 
       default:
-        break;
+        return null;
     }
   };
 
-  return <TestCard className={styles.question}>QuestionItem</TestCard>;
+  return (
+    <TestCard className={styles.question}>
+      <h5 className={styles.title}>
+        {question.title}
+        {question.required && <span className="required"> *</span>}
+      </h5>
+
+      {renderQuestionContent(question)}
+    </TestCard>
+  );
 };
 
 export default QuestionItem;
