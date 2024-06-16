e560d055c2a53129042394c8b98c811905682ca4
tonyWh1te <zmyshenko.valery@bk.ru>
2024-06-17 00:41:18 +0300

    feat: added test blocking on passing

--- src/modules/ComponentList/components/TestListItem/TestListItem.jsx
+++ src/modules/ComponentList/components/TestListItem/TestListItem.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { XCircleIcon } from '@heroicons/react/24/solid';
+import { XCircleIcon, CheckCircleIcon } from '@heroicons/react/24/solid';
 import { selectCurrentUser } from '../../../Auth';
 import { setSelectedTest } from '../../slice/testsSlice';
 import { useDeleteTestMutation } from '../../api/testApiSlice';
@@ -110,7 +110,13 @@ const TestListItem = ({ test, toggleModal }) => {
           <h3 className={styles.title}>{`${test.title} (${convertDate(test.created_at)})`}</h3>
           <p className={styles.description}>{test.description}</p>
         </div>
-        <div className={styles.buttons}>{renderTestButtons(role)}</div>
+        {test.is_passed ? (
+          <div className="z-10">
+            <CheckCircleIcon className={styles.icon} />
+          </div>
+        ) : (
+          <div className={styles.buttons}>{renderTestButtons(role)}</div>
+        )}
       </div>
     </div>
   );
